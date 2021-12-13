50e213c666d968f628141c92c47c5caa9cced098
diegodh1 <45396645+diegodh1@users.noreply.github.com>
2021-12-13 16:37:57 -0500

    Update JpaConfiguration.java

--- src/main/java/configuration/JpaConfiguration.java
+++ src/main/java/configuration/JpaConfiguration.java
@@ -2,15 +2,7 @@ package configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
@@ -24,6 +16,7 @@ public class JpaConfiguration {
         dataSource.setPassword("12345");
         dataSource.setUrl(
                 "jdbc:mysql://localhost:3306/exampledb?createDatabaseIfNotExist=true");
+
 
         return dataSource;
     }
